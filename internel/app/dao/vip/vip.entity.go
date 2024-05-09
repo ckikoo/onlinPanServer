4614364171d27c98621068f039e715ca078443d7
ckikoo <lj5683w@gmail.com>
2024-05-09 22:40:55 +0800

    1

--- internel/app/dao/vip/vip.entity.go
+++ internel/app/dao/vip/vip.entity.go
@@ -6,17 +6,8 @@ import (
 	"gorm.io/gorm"
 )
 
-// VipPackage 定义了VIP套餐的结构
-type VipPackage struct {
-	ID         int
-	Name       string
-	SpaceSize  int    // 空间大小，以MB为单位
-	SpeedLimit int    // 速度大小，以Mbps为单位
-	Status     string // 状态，例如 "激活"、"未激活"
-}
-
 type Vip struct {
-	ID         int     `gorm:"primaryKey" json:"id" form:"id"`                        // 主键
+	gorm.Model
 	SpeedLimit uint64  `gorm:"column:speedLimit" json:"speedLimit" form:"speedLimit"` // 下载限制速度
 	SpaceSize  uint64  `gorm:"column:spaceSize" json:"spaceSize" form:"spaceSize"`    // 容量限制
 	Show       bool    `gorm:"column:show" json:"show" form:"show'"`                  // 状态，可能表示是否激活
